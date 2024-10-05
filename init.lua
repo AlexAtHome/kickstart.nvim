973c352321d064d01f52a4afac61b5f246232b4c
Alex Bolotskov <mamoruuu@proton.me>
2024-10-05 18:35:59 +0500

    tsserver -> ts_ls

--- init.lua
+++ init.lua
@@ -630,8 +630,8 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        ts_ls = {
           root_dir = util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json'),
           on_attach = function(client, _)
             client.server_capabilities.document_formatting = false
