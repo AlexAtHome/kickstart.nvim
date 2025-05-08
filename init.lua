47a3aa848c2684c08f814247d7a32b55e189c0a4
Alex Bolotskov <mamoruuu@proton.me>
2025-05-08 10:53:25 +0500

    auto completion keys

--- init.lua
+++ init.lua
@@ -923,7 +923,10 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'enter',
+        preset = 'default',
+
+        ['<c-j>'] = { 'select_next', 'fallback' },
+        ['<c-k>'] = { 'select_prev', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
