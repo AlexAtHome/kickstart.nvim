853cab1115c78ccd5408b05cdece3eed80daf26a
Alexander Bolotskov <mamoruuu@proton.me>
2026-02-28 18:19:58 +0500

    upd

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,32 +3,21 @@
 --
 -- See the kickstart.nvim README for more information
 
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.expandtab = false
-
 return {
-  {
-    'tpope/vim-fugitive',
-    lazy = false,
-  },
-  {
-    '3rd/image.nvim',
-    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-    opts = {
-      processor = 'magick_cli',
-    },
-  },
-  {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_background = 'hard'
-      vim.cmd.colorscheme 'gruvbox-material'
-    end,
-  },
+	{
+		'tpope/vim-fugitive',
+		lazy = false,
+	},
+	{
+		'3rd/image.nvim',
+		lazy = true,
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			processor = 'magick_cli',
+			html = {
+				enabled = false,
+			},
+		},
+	},
 }
+-- vim: ts=2 sts=2 sw=2 et
