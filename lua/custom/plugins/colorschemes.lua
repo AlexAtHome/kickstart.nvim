c24bd165f8021a1f3a9cc73a8edd2edb02735305
Alex Bolotskov <mamoruuu@proton.me>
2026-09-01 16:42:11 +0500

    colorscheme change

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,3 +1,5 @@
-vim.pack.add { 'https://github.com/scottmckendry/cyberdream.nvim' }
-require('cyberdream').setup {}
-vim.cmd("colorscheme cyberdream")
+vim.pack.add({
+  "https://github.com/oskarnurm/koda.nvim",
+})
+require("koda").setup({ transparent = true })
+vim.cmd("colorscheme koda-moss")
